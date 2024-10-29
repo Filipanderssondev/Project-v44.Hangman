9f7d8701d2aa32f39f36ec3ea3db751d51bbb687
Chengjun724 <yanchengjun521y@gmail.com>
2024-10-29 18:32:01 +0100

    Sprint1 Yan 3.1 and randomword

--- Hangmantwist.cpp
+++ Hangmantwist.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <cstdlib>
 #include <cctype>
+#include <ctime>
+
 using namespace std;
+
+string getrandomword(const string& filename)
+{   
+    ifstream file(filename);
+    vector<string> words;
+    string word;
+
+    if (file.is_open())   
+    {
+        while (file >> word)
+        {
+            if(word.length() == 5)
+            {
+            words.push_back(word);
+            }
+        }
+        file.close();
+    }   else
+        {
+            cerr<< "Unable to open file: " << filename << endl;
+            return "";
+        }
+    
+    if (!words.empty()) {
+        int randomIndex = rand() % words.size();
+        return words[randomIndex];
+    } else {
+        std::cerr << "No 5-letter words found in the file." << std::endl;
+        return "";
+    }
+}   
+
+
+
 
 vector<char> Getuserconsonant()
 {  
@@ -9,8 +47,8 @@ vector<char> Getuserconsonant()
     char getconsonant;
         
     cout << "Please enter a letter(enter '0' to stop)" << endl;
-    while (true) //Loop kör tills player ange 0
-    {
+    while (true){ //Loop kör tills player ange 0
+    
         cin >> getconsonant;
         if (getconsonant == '0')
         {
@@ -28,7 +66,16 @@ vector<char> Getuserconsonant()
 }
         
 int main()
-{  
+{   
+    srand(static_cast<unsigned int>(time(nullptr)));
+    string filename = "words.txt";
+    string randomword = getrandomword(filename);
+    if (!randomword.empty()) 
+    {
+        std::cout << "Random 5-letter word: " << randomword << std::endl;
+    }
+
+   
     vector<char>userconsonant = Getuserconsonant();
     cout << "All letters entered: ";
     for (char consonant : userconsonant) //skriver ut alla bokstaver som har sparat i vector-consonant

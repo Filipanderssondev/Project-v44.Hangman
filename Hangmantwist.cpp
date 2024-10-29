f22b0c0de1a00f9593163ab8db2b432593d8cfc2
VSilkis <victor.silkisberg@chasacademy.se>
2024-10-29 10:59:01 +0100

    Added comments for easier understanding

--- Hangmantwist.cpp
+++ Hangmantwist.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 void FunctionRules();
-
+    // Add an output for the rules (Level 1 and Level 2)
 int FunctionEnterconsonant();
-
+    //Input a character and save it to a variable
 int FunctionEnterword();
-
+    //Input a word(string),  and save it
 int FunctionUsedconsonant();
-
+    //Compare to stored characters
 int FunctionUsedwords();
-
+    //Compare to stored words
 int FunctionWordsize();
-
+    //Compare the input word to the size of hidden word (==5)
 int FunctionSwitchfromlvl1tolvl2();
-
-int FUnctionQuitgame();
-
+    //Output the switch from Level 1 to Level 2
+int FunctionQuitgame();
+    //Command to quit the game (Boolean to true)
 int FunctionCorrectanswer();
-
+    //Compare the input word to the hidden word
 int main()
 
 {

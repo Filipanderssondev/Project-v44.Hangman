b35f01ce26a05675484f414ef916c3e1595f5245
Filip Andersson <andersson.filip98@gmail.com>
2024-10-30 12:08:41 +0100

    Implemented a workinng printMainMenu function

--- Hangmantwist.cpp
+++ Hangmantwist.cpp
@@ -4,8 +4,36 @@
 #include <cstdlib>
 #include <cctype>
 #include <ctime>
-
 using namespace std;
+
+void printMainMenu()
+{   
+    //Opens the file in reading mode
+    ifstream f("README.txt");
+
+    //Checks if the file is opened correctly
+    if (!f.is_open())
+    {
+        cerr << "Error message!: Couldnt open the file!";
+        return;
+    }
+
+    //String variable to to store the read data
+    string s;
+
+    // Read each line of the file and print it to the
+    // Standard output stream till the whole file is 
+    // Completely read
+    while (getline(f, s))
+    {
+        cout << s << endl;
+    }
+
+    //Close the file
+    f.close();
+    return;
+    //I got help from Geeks4Geeks to solve this 
+}
 
 bool FunctionUsedconsonant(const vector<char>& consonants, char userconsonant) {
     //Checks the consonants with stored ones to find duplicates
@@ -70,6 +98,7 @@ char Getuserconsonant()
         
 int main()
 {   
+    printMainMenu();
     srand(static_cast<unsigned int>(time(nullptr)));
     string filename = "words.txt";
     string randomword = getrandomword(filename);
